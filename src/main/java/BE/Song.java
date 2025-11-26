e6ca4ec4bad4b26fa54463ce9c82bc616b2c1137
MADS-THINKPAD\madst <madsot01@easv365.dk>
2025-11-26 13:09:34 +0100

    added to MyTunesDAO_DB, only need controllres and model now

--- src/main/java/BE/Song.java
+++ src/main/java/BE/Song.java
@@ -2,32 +2,59 @@ package BE;
 
 public class Song {
 
+    private int id;
     private String title;
     private String artist;
     private String category;
     private double time;
-    public Song(String title, String artist, String category, double time) {
+
+
+    public Song(int id, String title, String artist, String category, double time) {
+        this.id = id;
         this.title = title;
         this.artist = artist;
         this.category = category;
         this.time = time;
     }
-    public String getTitle(){return title;}
 
-    public String getArtist(){return artist;}
+    public String getTitle() {
+        return title;
+    }
 
-    public String getCategory(){return category;}
+    public String getArtist() {
+        return artist;
+    }
 
-    public double getTime(){return time;}
+    public String getCategory() {
+        return category;
+    }
 
-    public void setTitle(String title){this.title = title;}
+    public double getTime() {
+        return time;
+    }
 
-    public void setArtist(String artist){this.artist = artist;}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-    public void setCategory(String category){this.category = category;}
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
 
-    public void setTime(double time){this.time = time;}
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
 
     @Override
-    public String toString(){return title + ": " + artist + " " + category + " ("+time+")";}
+    public String toString() {
+        return id + ": " + title + " " + artist + " " + category + " (" + time + ")";
+    }
+
+    public int getId() {
+        return id;
+    }
 }
--- src/main/java/DAL/db/MyTunesDAO_DB.java
+++ src/main/java/DAL/db/MyTunesDAO_DB.java
@@ -6,9 +6,7 @@ import BE.Song;
 import DAL.IMyTunesDataAccess;
 // Java imports
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,12 +27,13 @@ public class MyTunesDAO_DB implements IMyTunesDataAccess {
 
             while (rs.next()) {
                 //Map DB row to Song object
+                int id = rs.getInt("Id");
                 String title = rs.getString("Title");
                 String artist = rs.getString("Artist");
                 String category = rs.getString("Category");
                 double time = rs.getDouble("Time");
 
-                Song song = new Song(title, artist, category, time);
+                Song song = new Song(id, title, artist, category, time);
                 allSongs.add(song);
             }
         }
@@ -44,17 +43,52 @@ public class MyTunesDAO_DB implements IMyTunesDataAccess {
 
     @Override
     public Song createSongs(Song newSong) throws Exception {
-return null;
+        String sql = "INSERT INTO dbo.Song (Title, Artist, Category, Time) VALUES (?, ?, ?, ?)";
+        try (Connection conn = databaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, newSong.getTitle()); // det her er placeholders, altså 1: Title 2: Artist 3: category 4: time
+            stmt.setString(2, newSong.getArtist());
+            stmt.setString(3, newSong.getCategory());
+            stmt.setDouble(4, newSong.getTime());
+
+            stmt.executeUpdate(); // sends the command to the database
+        }
+        return newSong;
     }
 
     @Override
     public void updateSongs(Song song) throws Exception {
+        String sql = "UPDATE Song SET Title = ?, Artist = ?, Category = ?, Time = ? WHERE Id = ?";
 
+        try (Connection conn = databaseConnector.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, song.getTitle());
+            stmt.setString(2, song.getArtist());
+            stmt.setString(3, song.getCategory());
+            stmt.setDouble(4, song.getTime());
+
+            stmt.setInt(5, song.getId()); //her identificere vi hvilken row der ska opdateres.
+
+            stmt.executeUpdate();
+        }
     }
 
     @Override
     public void deleteSongs(Song song) throws Exception {
+        String sql = "DELETE FROM dbo.Song WHERE ID = ?;";
 
+        try (Connection conn = databaseConnector.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(sql))
+        {
+            stmt.setInt(1,song.getId());
+
+          stmt.executeUpdate();
+        }
+        catch (SQLException ex)
+        {
+            throw new Exception("Could not get songs from database.",ex);
+        }
     }
 
     @Override
